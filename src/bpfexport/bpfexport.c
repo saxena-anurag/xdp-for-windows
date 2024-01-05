92075806adff5f1f00bdac5451eda00a6ca89c64
Anurag Saxena <anusa@microsoft.com>
2024-01-05 10:08:23 -0800

    backup

---
+++ src/bpfexport/bpfexport.c
@@ -0,0 +1,60 @@
+//
+// Copyright (c) Microsoft Corporation.
+//
+
+#include <stdio.h>
+#include <windows.h>
+//
+// Work around various eBPF compilation bugs.
+//
+#define USER_MODE 1
+#pragma warning(push)
+#pragma warning(disable:4005) // 'WIN32_LEAN_AND_MEAN': macro redefinition
+#include <ebpf_store_helper.h>
+#pragma warning(pop)
+#undef USER_MODE
+#include <wtc.h>
+#include <wtc_guids.h>
+#include <wtcstore.h>
+
+INT
+__cdecl
+main(
+    INT argc,
+    CHAR* argv[]
+)
+{
+    ebpf_result_t result;
+    int exit_code = 0;
+
+    if (argc == 2 && !_strcmpi("--clear", argv[1])) {
+        for (uint32_t i = 0; i < RTL_NUMBER_OF(EbpfTcSectionInfo); i++) {
+            result = ebpf_store_delete_section_information(&EbpfTcSectionInfo[i]);
+            if (result != EBPF_SUCCESS) {
+                fprintf(stderr, "ebpf_store_delete_section_information failed: %u\n", result);
+                exit_code = -1;
+            }
+        }
+
+        result = ebpf_store_delete_program_information(&EbpfTcProgramInfo);
+        if (result != EBPF_SUCCESS) {
+            fprintf(stderr, "ebpf_store_delete_program_information failed: %u\n", result);
+            exit_code = -1;
+        }
+    }
+    else {
+        result = ebpf_store_update_section_information(&EbpfTcSectionInfo[0], RTL_NUMBER_OF(EbpfTcSectionInfo));
+        if (result != EBPF_SUCCESS) {
+            fprintf(stderr, "ebpf_store_update_section_information failed: %u\n", result);
+            exit_code = -1;
+        }
+
+        result = ebpf_store_update_program_information(&EbpfTcProgramInfo, 1);
+        if (result != EBPF_SUCCESS) {
+            fprintf(stderr, "ebpf_store_update_program_information failed: %u\n", result);
+            exit_code = -1;
+        }
+    }
+
+    return exit_code;
+}
